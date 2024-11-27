12f5a4856fbfc9b16c286cf2126d43065f10df63
OYAGIRIKU <riku01163@gmail.com>
2024-11-27 18:12:12 +0900

    [Update]ChestFloorScene 宝箱があるフロアの処理改良。

--- Assets/Script/ChestFloorSys.cs
+++ Assets/Script/ChestFloorSys.cs
@@ -1,14 +1,100 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChestFloorSys : MonoBehaviour
 {
+    public Camera maincamera = null;
+
+    bool runStratFlag = false;
+    bool chestEndFlag = false;
+    bool floorEndFlag = false;
+    [SerializeField]
+    Image fade = null;
+    
+
+    [SerializeField]
+    Vector3 cameraMoveSpeed = Vector3.zero;
+
+    [SerializeField]
+    GameObject chestObj = null;
+
+    public TextMeshProUGUI windowMes = null;
+
     void Start()
     {
-        
+        Init();
     }
 
+    void Init()
+    {
+        windowMes.text = "探索中";
+    }
     void Update()
     {
-        
+        KeyIn();
+        if (runStratFlag)
+        {
+            if(maincamera.transform.position.x >= 15)
+            {
+                windowMes.text = "宝箱を見つけた！\n 中にはパーツが入っていた！";
+                chestObj.SetActive(false);
+                runStratFlag = false;
+                StartCoroutine(ChestWait());
+            }
+            else
+            {
+                maincamera.transform.position += cameraMoveSpeed * Time.deltaTime;
+            }
+        }
+        if (chestEndFlag)
+        {
+            if (maincamera.transform.position.x <= 30)
+            {
+                windowMes.text = "探索中";
+                maincamera.transform.position += cameraMoveSpeed * Time.deltaTime;
+            }
+            else
+            {
+                windowMes.text = "扉を見つけた！ \n次の階に進もう";
+                StartCoroutine(FloorEnd());
+            }
+        }
+
+        //フェードアウト処理
+        if(floorEndFlag)
+        {
+            Color32 fadecolor = fade.color;
+            if (fadecolor.a <= 254)
+            {
+                fadecolor.a += (byte)(255 * Time.deltaTime);
+            }
+            else
+            {
+                floorEndFlag = false;
+            }
+            fade.color = fadecolor;
+        }
+    }
+
+    void KeyIn()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            runStratFlag = true;
+        }
+    }
+
+    IEnumerator ChestWait()
+    {
+        yield return new WaitForSeconds(1.5f);
+        chestEndFlag = true;
+    }
+    IEnumerator FloorEnd()
+    {
+        yield return new WaitForSeconds(1.0f);
+        chestEndFlag = false;
+        floorEndFlag = true;
     }
 }
