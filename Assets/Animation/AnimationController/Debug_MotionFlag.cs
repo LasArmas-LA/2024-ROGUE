c3526105a49476ce9a2783ec3d48edfa82648ae8
ユーãNFG-Note <game08minecraft17nespi@gmail.com>
2024-12-16 14:44:06 +0900

    update ディアモーションの命名変更とパラメーター実装

---
+++ Assets/Animation/AnimationController/Debug_MotionFlag.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Debug_MotionFlag : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
